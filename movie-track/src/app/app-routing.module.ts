275f85fcb826565a83dc7681911c229d01e48fef
Caio Oliveira Nascimento <caio.onascimento@hotmail.com>
2024-12-04 12:27:19 -0300

    add estilos

--- movie-track/src/app/app-routing.module.ts
+++ movie-track/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TelaCadastroUsuarioComponent } from './componentes/tela-cadastro-usuario/tela-cadastro-usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cadastro', component: TelaCadastroUsuarioComponent,},
+{ path: '', redirectTo: '/cadastro', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- movie-track/src/app/app.module.ts
+++ movie-track/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { TelaCadastroUsuarioComponent } from './componentes/tela-cadastro-usuario/tela-cadastro-usuario.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelaCadastroUsuarioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
